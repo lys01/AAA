be278e9f6e056b5eaf611341740e312d9afb2866
seamly888 <73085477+seamly888@users.noreply.github.com>
2021-07-04 20:49:46 +0800

    Add files via upload

--- jd_europeancup.js
+++ jd_europeancup.js
@@ -1,11 +1,11 @@
 /*
 更新地址：https://raw.githubusercontent.com/Wenmoux/scripts/wen/jd/jd_ddnc_farmpark.js
+号1默认给wuzhi助力 后续号给号1
+入口 https://lzdz-isv.isvjcloud.com/dingzhi/hisense/europeancup/activity/7431935?activityId=901100032442101)
 ============Quantumultx===============
 [task_local]
 #欧洲狂欢杯
-号1默认给我助力 后续号给号1
-入口 https://lzdz-isv.isvjcloud.com/dingzhi/hisense/europeancup/activity/7431935?activityId=901100032442101&shareUuid=b7f58330cb0844b485afacbdea3c7bca&adsource=null&initHash=/home&shareuserid4minipg=8A+Mf3SBYE8spQtvzQ2VLE7oeVP9kq2pYSH90mYt4m3fwcJlClpxrfmVYaGKuquQkdK3rLBQpEQH9V4tdrrh0w==&shopid=undefined&lng=107.146945&lat=33.255267&sid=cad74d1c843bd47422ae20cadf6fe5aw&un_area=8_573_6627_52446)
-55 59 9  * * ? https://raw.githubusercontent.com/Wenmoux/scripts/wen/jd/jd_europeancup.js, tag=欧洲狂欢杯, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+57 59 9  * * * https://raw.githubusercontent.com/Wenmoux/scripts/wen/jd/jd_europeancup.js, tag=欧洲狂欢杯, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 */
 const $ = new Env('狂欢欧洲杯');
 //Node.js用户请在jdCookie.js处填写京东ck;
@@ -30,8 +30,8 @@ if ($.isNode()) {
 
 const JD_API_HOST = `https://api.m.jd.com/client.action`;
 message = ""
-$.actid = " "
-$.shareuuid = " " //俺的助力码 
+$.actid = "901100032442101"
+$.shareuuid = []
     !(async () => {
         if (!cookiesArr[0]) {
             $.msg($.name, '【提示】请先获取cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {
@@ -68,6 +68,10 @@ $.shareuuid = " " //俺的助力码
                 await getUserInfo()
                 await getUid()
                 if ($.cando) {
+                    await exchange(cupExid) //兑换
+                    await exchange(cupExid) //兑换
+                    await exchange(cupExid) //兑换
+                    await exchange(cupExid) //兑换
                     await exchange(cupExid) //兑换
                     await getinfo()
                     taskList = $.taskList
@@ -234,6 +238,16 @@ function getMyPin() {
                         cookie = `${cookie} AUTH_C_USER=${$.pin};`
                         $.nickname = data.data.nickname
                         console.log(`欢迎回来~  ${$.nickname}`);
+                        if ($.isNode()){
+                            for (let ck of resp['headers']['set-cookie']) {
+                                cookie = `${cookie}; ${ck.split(";")[0]};`
+                            }
+                        }
+                        else {
+                            for (let ck of resp['headers']['Set-Cookie'].split(',')) {
+                                cookie = `${cookie}; ${ck.split(";")[0]};`
+                            }
+                        }
                     }
                 }
             } catch (e) {
